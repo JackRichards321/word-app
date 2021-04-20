1c2db4e06fb635b0de69d7ea11644ac0cf598e1b
Jack Richards <sjackrichards88@gmail.com>
2021-04-20 19:50:11 -0400

    committed client folder

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 const unirest = require("unirest");
 const API_KEY = "2cc1f01d-bf8b-4c04-9104-745661e58089";
+// import path library
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// ...
 
 app.get('/api/associations/:word', (req, res) => {
 	const word = req.params.word;
@@ -18,20 +25,12 @@ app.get('/api/associations/:word', (req, res) => {
 	});
 });
 
-app.listen(port, () => {
-  console.log(`word-app listening on port ${port}`);
-});
-
-// import path library
-const path = require('path');
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// ...
-
 // put this AFTER the other `app.get(...`
 // for any request that doesn't match one above, send back React's index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
 });
